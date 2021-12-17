ea0ff8525dd7f2e434dd8e061e44039cef4dbeec
Salomé <salome.pechin@gmail.com>
2021-12-17 12:10:30 +0100

    ajout liste tâches + détails

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import react from 'react';
+import NewTaskList from './components/NewTaskList';
+import NewTask from './components/NewTask';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const initList = [
+  {
+    id: 1,
+    name: 'Task 1',
+    desc: 'desc',
+    date: 'date'
+  },
+  {
+    id: 2,
+    name: 'Task 2',
+    desc: 'desc',
+    date: 'date'
+  }
+];
 
 function App() {
+
+  const [list, setList] = useState(initList);
+  const [name, setName] = useState('');
+  const [desc, setDesc] = useState();
+  const [date, setDate] = useState();
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleAdd() {
+    if (name != '') {
+      const newList = list.concat({ nameid: uuidv4(), name, desc, date  });
+      setList(newList);
+    } else {
+      alert('Veuillez entrer un autre titre pour votre tâche.');
+    }
+    setName('');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Todo List App</h1>
       </header>
+
+      <input type="text" placeholder="Nom de la tâche" value={name} onChange={handleChange} /><br /><br />
+      <input type="text" placeholder="Description" value={desc} onChange={handleChange} /><br /><br />
+      <input type="text" placeholder="Date limite" value={date} onChange={handleChange} /><br /><br />
+      <button type="button" onClick={handleAdd}>
+        Créer tâche
+      </button>
+
+      <NewTask
+        name={name}
+        desc={desc}
+        date={date} />
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/components/NewTaskList.js
@@ -0,0 +1,15 @@
+import './List.css';
+import React from "react";
+import { useState } from 'react';
+import NewTask from './NewTask';
+
+function NewTaskList(listName, initList) {
+
+    return (
+        <div className="NewTaskList">
+
+        </div>
+    );
+}
+
+export default NewTaskList;
